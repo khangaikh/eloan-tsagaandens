1392f6b8a76cd9ecf82c51566cc25a4d20560a4c
khangai <khangai@alchemist.mn>
2015-08-21 20:16:06 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once 'includes/Twig/Autoloader.php';
+    require_once "config.php";
+    use Parse\ParseObject;
+    use Parse\ParseQuery;
+    
+    //register autoloader
+    Twig_Autoloader::register();
+    //loader for template files
+    $loader = new Twig_Loader_Filesystem('templates');
+    //twig instance
+    $twig = new Twig_Environment($loader, array(
+        'cache' => 'cache',
+    ));
+    //load template file
+    $twig->setCache(false);
+    $template = $twig->loadTemplate('index.html');
+    
+    $query = new ParseQuery("loan");
+    $query->equalTo("status",1);
+    $results = $query->find();
+    //render a template
+    echo $template->render(array('title' => 'eLoan'));
+?>
+
